1ef360f306d744f0171037e76a865b5b398de022
AngeloCoats <angelo.coats@gmail.com>
2019-07-23 11:48:52 +0200

    Add files via upload

---
+++ inc/nav.php
@@ -0,0 +1,29 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light border-bottom nav-custom">
+    <div class="container">
+    <a class="navbar-brand" href="#">Angelo Coats</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Vaardigheden</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Doelen</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Voortgang</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+        </li>
+        
+        </ul>
+    </div>
+    </div>
+    <span class="border-bottom"></span>
+</nav>
